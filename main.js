4a0cae239ccce2f122e35e6a65d8c0cd810d9029
819605834 <124411012+819605834@users.noreply.github.com>
2026-06-18 19:59:41 +0000

    Created getValue

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 // Write your code below
+function getValue(obj, key) {
+    if (obj === undefined || obj === null) return 'null';
+    if (!Object.hasOwn(obj, key)) return 'undefined';
+
+    return obj[key];
+}
+var c = 'ee'
+var car = {
+  name: 'Tesla'
+};
+
+var value = getValue(c, 'name');
+console.log(value);
+value = getValue(car, 'name');
+console.log(value);
